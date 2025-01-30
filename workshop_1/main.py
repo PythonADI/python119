1357a1bbb5322843c4a7616e70639a47bcfbcd7d
tavkhelidzeluka <tavkhelidzeluka@gmail.com>
2025-01-30 21:07:11 +0400

    Add workshop 1 code

---
+++ workshop_1/main.py
@@ -0,0 +1,11 @@
+# assignment operator
+money_in_wallet = 100
+sandwich_price = 10
+
+print("I have", money_in_wallet, "$")
+print("Sandwich Price:", sandwich_price, "$")
+
+# assignment operator
+# money_in_wallet = money_in_wallet - sandwich_price
+money_in_wallet -= sandwich_price
+print("I have", money_in_wallet, "$")
