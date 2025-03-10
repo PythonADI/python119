bfe47fd9bfcca4f5faa850aea3cb97373201dfa0
tavkhelidzeluka <tavkhelidzeluka@gmail.com>
2025-03-10 21:01:06 +0400

    Add workshop 11 code

--- workshop_10/blog/admin.py
+++ workshop_10/blog/admin.py
@@ -1,5 +1,3 @@
 from django.contrib import admin
-from core.models import Product
 
 # Register your models here.
-admin.site.register([Product])
--- workshop_10/core/views.py
+++ workshop_10/core/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+from core.models import Product
 
 # Create your views here.
 def home_view(request):
-    return render(request, "index.html")
+    return render(
+        request,
+        "index.html",
+        {
+            "products": Product.objects.all()
+        }
+    )
+
+
+def about_view(request):
+    return render(request, "about.html")
